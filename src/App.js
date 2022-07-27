6b6447ffc31015e9bfa24de387905fa688c6eb6f
Liqingcathy <cathy.glc@gmail.com>
2022-07-26 18:04:21 -0700

    make get vehicle id api call work with form

--- src/App.js
+++ src/App.js
@@ -6,32 +6,63 @@ import Searchbar from './Components/search/Searchbar';
 import InputForm from './Components/UI/InputForm';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://127.0.0.1:5000';
 
 function App() {
   const [userData, setUserData] = useState([]);
-  
+  const [vehicleMakeName, setVehicleMakeName] = useState(''); //car brand
+  const [vehicleMakeId, setVehicleMakeId] = useState('');
+  const [vehicleModelId, setVehicleModelId] = useState('');
+  const [vehicleModelName, setVehicleModelName] = useState('');
 
-  const getEstimateData = () => {
-    let estimated_g, estimated_lb;
-    axios.get(`${API_URL}/estimate`)
-    .then((response) => {
-        setUserData(response.data)
-        estimated_g = response.data['carbon_g'];
-        estimated_lb = response.data['carbon_lb'];
-
-        console.log(estimated_g, estimated_lb);
-    }).catch((error) => {
-      console.log('error ', error);
-    })
+  //1.call vehicle make url get list of dict with vehile brand name
+  const getVehicleMakes = (brandName) => {
+    console.log('inside of api func');
+    axios.get(`${API_URL}/vehicle_makes`)
+         .then((response) => {
+          // console.log(response.data)
+          for (let brand of response.data){
+            // console.log(brand)
+            if (brand['data']['attributes']['name'] === brandName){
+                setVehicleMakeName(brandName);
+                setVehicleMakeId(brand['data']['id']);
+            }
+          }
+          console.log('vehicleMakeName', vehicleMakeName);
+          console.log('vehicleMakeID', vehicleMakeId);
+         })
+         .catch((error) => {
+          console.log('error detected', error);
+         })
   }
+
+  // const getVehicleModels = (vehicleMakeId) => {
+  //   axios.get(`${API_URL}/vehicle_makes/${vehicleMakeId}/vehicle_models`)
+  //       .then((response) => {
+  //         setVehicleModelName(response.data['data']['attributes']['name']);
+  //         setVehicleModelId(response.data['data']['id']);
+  //       })
+      
+  // }
+  // const getEstimateData = (data) => {
+  //   let estimated_g, estimated_lb;
+  //   axios.get(`${API_URL}/estimate`, data)
+  //   .then((response) => {
+  //       setUserData(response.data)
+  //       estimated_g = response.data['carbon_g'];
+  //       estimated_lb = response.data['carbon_lb'];
+  //       console.log(estimated_g, estimated_lb);
+  //   }).catch((error) => {
+  //     console.log('error ', error);
+  //   })
+  // }
 
   return (
     <div className="App">
       <Navbar />
       <Header />
       <Searchbar />
-      <InputForm handleFormSubmit={getEstimateData}/>
+      <InputForm  handleVehicleMakesSubmit={getVehicleMakes}/>
     </div>
   );
 }
--- src/Components/UI/InputForm.js
+++ src/Components/UI/InputForm.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import './InputForm.css';
 
-const inputData = {type:'', distance_unit:'', distance_value:0, vehicle_model_id:'' }
+const inputData = {type:'vehicle', brand_name: '', model_name: '', distance_unit:'', distance_value:0, vehicle_model_id:'' }
 
 const InputForm = (props) => {
   //passed down userData
   const [userData, setInputData] = useState(inputData);
-  
+  const [page, setPages] = useState(0);
+  const FormTitle = ['Vehicle Brand', 'Vehicle Model', 'Vehicle Model ID', 'Distance Unit', 'Distance Value'];
+
   //when user enters input value is changed, handles event
   const handleFormInput = (event) => {
     const target = event.target;
@@ -17,27 +19,64 @@ const InputForm = (props) => {
     //type, unit, value, model id update
     newInputData[name] = value;
     setInputData(newInputData);
+    console.log('new input data', userData);
   }
 
-  const handleNextStepInput = () => {
-    
-  }
+  // const handleNextStepInput = (event) => {
+  //   event.preventDefault();
+  //   props.handleVehicleMakesSubmit(userData.brand_name);
+  //   setSteps(step + 1);
+  //   console.log(step);
+  // }
 
-  const handleInputSubmit = (event) => {
+  // const handlePrevStepInput = (event) => {
+  //   event.preventDefault();
+  //   props.handleVehicleMakesSubmit(userData.brand_name);
+
+  //   if(step !== 0){
+  //     setSteps(step - 1);
+  //     }
+  // }
+
+  
+  const handleInputSubmission = (event) => {
     event.preventDefault();
-    props.handleInputSubmit(userData);//update userdata and lift up to app.js
+    props.handleVehicleMakesSubmit(userData.brand_name);//update userdata and lift up to app.js
   }
 
 
   return (
     <div>
-      <form onSubmit={handleInputSubmit}>
-        {/* <label>Zipcode</label> */}
-        {/* <input type='text' /> <br/> */}
-        <label>Vehicle Type</label>
+      <h1>{FormTitle[page]}</h1>
+      <button>Prev</button>
+      <button onClick={() => {setPages((currPage) => currPage + 1)}}>Next</button>
+      {/* <form onSubmit={handleInputSubmission}> */}
+      {/* <label>Vehicle Brand</label>
+        <input 
+          type='text' 
+          required name='brand_name'
+          value={props.brand_name}
+          onChange={handleFormInput}
+          /> <br/> */}
+        {/* <button className="back" onClick={handlePrevStepInput}>prev</button> <br/>
+        <button className="next" onClick={handleNextStepInput}>next</button> <br/> */}
+      {/* <label>Vehicle Model</label>
         <input 
           type='text' 
           required name='type'
+          value={props.model_name}
+          onChange={handleFormInput}
+          /> <br/> */}
+        {/* <label>Vehicle Model ID</label>
+        <input 
+          type='text' 
+          required name='value'
+          value={props.vehicle_model_id} 
+          onChange={handleFormInput}/><br/>
+        <label>Vehicle Type</label>
+        <input 
+          type='text' 
+          name='type'
           value={props.type}
           onChange={handleFormInput}
           /> <br/>
@@ -58,11 +97,11 @@ const InputForm = (props) => {
           type='text' 
           required name='value'
           value={props.distance_value} 
-          onChange={handleFormInput}/><br/>
-         <input className="btn-estimate" type="submit" value="Get Estimation Now" />
+          onChange={handleFormInput}/><br/> */}
+         {/* <input className="btn-estimate" type="submit" value="Get Estimation Now" />
       </form>
-    </div>
-  )
+    </div> */}
+  </div>)
 }
 
 export default InputForm;
