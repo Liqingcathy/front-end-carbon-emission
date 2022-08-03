fe0aa8bfca5e455ef99ccb9e51903bfb5fc987e1
Liqingcathy <cathy.glc@gmail.com>
2022-08-02 17:47:55 -0700

    added to display content to search result component

--- src/Components/search/SearchResult.js
+++ src/Components/search/SearchResult.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link} from "react-router-dom";
 
-const SearchResult = ({title, url}) => {
+const SearchResult = ({title, url, content}) => {
   console.log('inside of result item');
   // console.log(title);
   return (
     // <li className="search-result-item"> <a href={url} >{title}</a></li>
-    <li className="search-result-item"> {title}</li>
+    <ul className="search-result-items"> 
+      <li className="search-result-titles"> <Link to={url}>{title}</Link> </li>
+      <li className="search-result-content" > {content}</li>
+    </ul>
+
   )
 }
 
